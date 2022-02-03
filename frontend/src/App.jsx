3a67c94f09f3e52f1b7de52c66938ad76cd18480
Alex Nunez <nunezaleks@gmail.com>
2022-02-03 15:16:12 -0800

    add routes for home, about and lessons page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,21 @@
-import React from 'react';
+import React from "react";
+import { Routes, Route, Link } from "react-router-dom"
 
-import Home from './components/home/Home';
-
+import Home from "./pages/home/Home";
+import Topbar from './components/Topbar';
+import About from "./pages/about/About";
+import Lessons from "./pages/lessons/Lessons";
 
 const App = () => {
-    return(
-        <div>
-            <Home />
-        </div>
-    )
-}
-
+  return (
+    <div>
+      <Topbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="lessons" element={<Lessons />} />
+      </Routes>
+    </div>
+  );
+};
 export default App;
