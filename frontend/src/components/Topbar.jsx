769f639eada73ef9ccaf997aad752898cbd680d4
Alex Nunez <nunezaleks@gmail.com>
2022-02-03 16:46:43 -0800

    add styling and contribute button

--- frontend/src/components/Topbar.jsx
+++ frontend/src/components/Topbar.jsx
@@ -7,7 +7,8 @@ import {
   IconButton,
   Box,
 } from "@mui/material";
-import { ThemeOptions } from "@mui/material";
+import { fontWeight } from "@mui/system";
+
 
 import { Link } from 'react-router-dom';
 import CSEDLogo from "../iconComponents/CSEDLogo";
@@ -18,38 +19,60 @@ const Topbar = () => {
     <div>
       <Box sx={{ flexGrow: 1 }}>
         <CssBaseline />
-        <AppBar position="relative" style={{ background: "#EEEEEE" }}>
+        <AppBar position="relative" style={{ background: "#FFFFFF" }}>
           <Toolbar>
           <IconButton component={Link} to={'/'}>
-          <CSEDLogo width={50} height={50}/>
+          <CSEDLogo width={50} height={50} />
           </IconButton>
-          
+          <h4 style={{
+            color: "black",
+            fontWeight: "bolder"
+            }}>
+              Open Lessons
+          </h4>
             <Typography
             align="center"
               variant="h6"
-              color="black"
-              marginRight="80px"
+              color="primary"
+              marginRight="110px"
               sx={{ flexGrow: 1 }}
             >
-              <Button 
-              style={{ color: "black"}}
+              <Button
+              style={{ 
+                color: "black",
+                textTransform: "none"
+              }}
               component={Link} to={'/'}
               >
                 home
               </Button>
               <Button 
-              style={{ color: "black"}}
+              style={{ 
+                color: "black",
+                textTransform: "none"
+              }}
               component={Link} to={'/about'}
               >
                 about
               </Button>
               <Button 
-              style={{ color: "black"}}
+              style={{ 
+                color: "black",
+                textTransform: "none"
+              }}
               component={Link} to={'/lessons'}
               >
                 lessons
               </Button>
             </Typography>
+              <Button
+              variant="contained"
+              style={{
+                backgroundColor: "black",
+                textTransform: "none"
+              }}
+              href="https://github.com/alexandernunez1/lms-for-ell"
+              >contribute</Button>
           </Toolbar>
         </AppBar>
       </Box>
